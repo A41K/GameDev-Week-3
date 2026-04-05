522a9d6d3031761ab3696f3a579690d2a3e9cc9c
A41K <arokandris@gmail.com>
2026-04-05 19:29:33 +0200

    v1.6 New LVL + Small changes
    - Started working on LvL 6
    - Added text for checkpoints when touching them so the player knows.
    - Made the wallsticks smaller cause they shouldn't be this big and it's not harder
    - The wallstick code remains (IDK HOW IT WAS DIFFERENT this code is verbatim the one before)
    - Changed how the checkpoint works

--- game-dev-week-3-(4.5)/scripts/checkpoint.gd
+++ game-dev-week-3-(4.5)/scripts/checkpoint.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
+@onready var checkpoint_label: Label = get_node_or_null("Label")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	if checkpoint_label:
+		checkpoint_label.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,4 +18,11 @@ func _process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		if checkpoint_label:
+			checkpoint_label.visible = true
 		GameController.set_respawn_point(global_position)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player") and checkpoint_label:
+		checkpoint_label.visible = false
